f5348256ff1b6f93aafd5c124a5328213b7b782e
samsagaz <monobedoya@gmail.com>
2019-10-20 08:27:07 -0500

    Nuevos Proyectos agregados a la solucion

---
+++ BaseFramework/AgroAppMVC/ServicioGranja/App_Code/Service.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using AgroAppDomain;
+
+// NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
+public class Service : IService
+{
+	public string GetData(int value)
+	{
+        Class1 class1 = new Class1();
+		return string.Format("You entered: {0}", value);
+	}
+
+	public CompositeType GetDataUsingDataContract(CompositeType composite)
+	{
+		if (composite == null)
+		{
+			throw new ArgumentNullException("composite");
+		}
+		if (composite.BoolValue)
+		{
+			composite.StringValue += "Suffix";
+		}
+		return composite;
+	}
+}
