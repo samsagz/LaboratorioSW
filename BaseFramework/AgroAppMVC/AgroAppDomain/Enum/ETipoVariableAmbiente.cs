b52bde9d2a1b2ad06943bfebf5ee5d4a1929b7a6
samsagaz <monobedoya@gmail.com>
2019-10-20 14:15:11 -0500

    Config

---
+++ BaseFramework/AgroAppMVC/AgroAppDomain/Enum/ETipoVariableAmbiente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgroAppDomain.Enum
+{
+    public enum ETipoVariableAmbiente
+    {
+        Temperatura = 1,
+        Humedad = 2,
+        Luz = 3
+    }
+}
---
+++ BaseFramework/AgroAppMVC/ServiciosGranja/IAgroAppService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace ServiciosGranja
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IAgroAppService" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IAgroAppService
+    {
+        [OperationContract]
+        [WebInvoke(UriTemplate = "/RegistrarValorGranja",
+            BodyStyle = WebMessageBodyStyle.WrappedResponse,
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json)]
+        RegistroResponse RegistrarValorGranja(RegistroRequest nuevoRegistro);
+    }
+
+    public class RegistroRequest
+    {
+        public string Fecha { get; set; }
+        public string VariableAmbiente { get; set; }
+        public double Valor { get; set; }
+        public int ModuloId { get; set; }
+    }
+
+    public class RegistroResponse
+    {
+        public string Mensaje { get; set; }
+        public List<Actuador> Actuadores { get; set; }
+    }
+
+    public class Actuador
+    {
+        public string NombreActuador { get; set; }
+        public bool Activar { get; set; }
+    }
+}
